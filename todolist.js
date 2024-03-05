86219eb825358e5051bf85f49fb7a2ff00c502fa
sergiojaa <119433399+sergiojaa@users.noreply.github.com>
2024-03-05 22:17:26 +0400

    Add files via upload

--- todolist.js
+++ todolist.js
@@ -1,2 +1,27 @@
-// gavaketeb da mere amas shevcvli//
-// mexute
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+ const [todoList, setTodoList] = useState([])
+ const [newTask, setNewTask] = useState('')
+  const handleChange = (event) =>{
+    setNewTask(event.target.value)
+  }
+  const addTask = () =>{
+    setTodoList( [...todoList, newTask])
+  }
+  return<div className='App'>
+    <div className='addTask'>
+      <input onChange={handleChange} />
+      <button onClick={addTask} >add task</button>
+    </div>    
+    <div className='list'>
+      {todoList.map((task) =>{
+        return <h1>{task}</h1>
+      } )}
+    </div>
+    
+    </div>
+}
+
+export default App;
